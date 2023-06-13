b30ecd563e5ff5fd5525bece3f6b1ffb61709d55
esraamamoun5 <esraamamoun5@gmail.com>
2023-06-13 20:47:08 +0300

    add card items and card section

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,6 +3,9 @@ import { Container } from "../components/shared";
 import { colors } from "../components/colors";
 import { FunctionComponent } from "react";
 import { StatusBar } from "expo-status-bar";
+import logo1 from "./../assets/cards/visa_white.png";
+import logo2 from "./../assets/cards/mc.png";
+import CardSection from "../components/Cards/CardSection";
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.graylight};
@@ -11,9 +14,33 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
+  const cardsData = [
+    {
+      id: 1,
+      accountNo: "3485757744",
+      balance: 20000.15,
+      alias: "Work Debit",
+      logo: logo1,
+    },
+    {
+      id: 2,
+      accountNo: "3485757703",
+      balance: 156.01,
+      alias: "Personal Prepaid",
+      logo: logo2,
+    },
+    {
+      id: 3,
+      accountNo: "3485757847",
+      balance: 5622.18,
+      alias: "School Prepaid",
+      logo: logo1,
+    },
+  ];
   return (
     <HomeContainer>
       <StatusBar style="dark" />
+      <CardSection data={cardsData} />
     </HomeContainer>
   );
 };
